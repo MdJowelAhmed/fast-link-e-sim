a59ff8da4315d8cb1ea195d3d4c322f968656a28
MdJowelAhmed <mdjowelahmed924@gmail.com>
2026-04-01 12:54:55 +0600

    update contry api add

---
+++ src/helpers/regionsApi.js
@@ -0,0 +1,22 @@
+import { baseApi } from "./baseApi";
+
+const regionsApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    getRegions: builder.query({
+      query: () => ({
+        url: "/country/regions",
+        method: "GET",
+      }),
+      providesTags: ["Region"],
+    }),
+    getCountriesBasedOnRegion: builder.query({
+      query: (region) => ({
+        url: `/country/${region}`,
+        method: "GET",
+      }),
+      providesTags: ["Region"],
+    }),
+  }),
+});
+
+export const { useGetRegionsQuery, useGetCountriesBasedOnRegionQuery } = regionsApi;
